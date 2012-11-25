d35d1db054d7f0931b97fea61fd56ec489818e89
Zhou Xianhua <xianhua.zhou@gmail.com>
2012-11-25 14:20:33 +0800

    fixed creating symlink in the "deploy setup" task

--- pac.sh
+++ pac.sh
@@ -256,7 +256,7 @@ function clean_old_releases {
     done
 }
 
-START_TIME=$(/bin/date +%s)
+START_TIME=$(date +%s)
 
 # deploy
 if [[ $1 = "deploy" ]]; then
@@ -264,12 +264,15 @@ if [[ $1 = "deploy" ]]; then
     if [[ $2 = "setup" ]]; then
         log ">> Deployment setup ..."
 
-        remote_cmd "/bin/mkdir -p ${SHARED_DIR}/cached-copy ${RELEASE_DIR}"
+        remote_cmd "mkdir -p ${SHARED_DIR}/cached-copy ${RELEASE_DIR}"
         for link in "${SHARED_DIRS[@]}"; do
             if [[ "${link}" == *"."* ]]; then
-                remote_cmd "echo > ${SHARED_DIR}/${link}"
+                linkfile="${SHARED_DIR}/${link}"
+                directory=`dirname ${linkfile}`
+                remote_cmd "mkdir -p ${directory}"
+                remote_cmd "echo > ${linkfile}"
             else
-                remote_cmd "/bin/mkdir -p ${SHARED_DIR}/${link}"
+                remote_cmd "mkdir -p ${SHARED_DIR}/${link}"
             fi
         done
     else
@@ -292,17 +295,17 @@ if [[ $1 = "deploy" ]]; then
         else
             run_rsync "deploy"
             if [[ $RUN = "Y" ]]; then
-                remote_cmd "/bin/cp -r -p ${SHARED_DIR}/cached-copy ${CURRENT_RELEASE}"
+                remote_cmd "cp -r -p ${SHARED_DIR}/cached-copy ${CURRENT_RELEASE}"
             fi
         fi
 
         # after the real deployment
         if [[ $RUN = "Y" ]]; then
             for dirlink in "${SHARED_DIRS[@]}"; do
-                remote_cmd "/bin/rm -rf ${CURRENT_RELEASE}/${dirlink}; /bin/ln -s ${SHARED_DIR}/${dirlink} ${CURRENT_RELEASE}/${dirlink}"
+                remote_cmd "rm -rf ${CURRENT_RELEASE}/${dirlink}; ln -s ${SHARED_DIR}/${dirlink} ${CURRENT_RELEASE}/${dirlink}"
             done
             run_hook "before_link"
-            remote_cmd "/bin/rm -f ${CURRENT_RELEASE_LINK}; /bin/ln -s ${CURRENT_RELEASE} ${CURRENT_RELEASE_LINK}"
+            remote_cmd "rm -f ${CURRENT_RELEASE_LINK}; ln -s ${CURRENT_RELEASE} ${CURRENT_RELEASE_LINK}"
             run_hook "after_link"
         fi
 
